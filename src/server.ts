74ab1ef921f050ea91e3530c2a0b4259a7514217
Mirko Lauff <bleiwolle@gmail.com>
2025-07-09 15:05:57 +0200

    Create endpoint for minifying Urls (#1)
    feature: add url shortener endpoint
    fix: correct endpoint types
    feature: add global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,48 @@
 import express from "express";
+import { nanoid } from "nanoid";
+import type { NextFunction, Request, Response } from "express";
+
+const nanoidAsync = async (length: number): Promise<string> =>
+  Promise.resolve(nanoid(length));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (_, res) => {
   res.send("Hello from Express and Bun!");
+});
+
+app.post(
+  "/api/shorten",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { url }: { url: string } = req.body || {};
+
+    if (!url) {
+      res.status(400);
+      const error = new Error("Missing URL in request body.");
+      return next(error);
+    }
+
+    try {
+      const shortUrl = await nanoidAsync(10);
+      res.json({ shortUrl });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+// Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.statusCode < 400) {
+    res.status(500);
+  }
+
+  const message = err.message || "Internal Server Error";
+
+  res.json({ error: { message } });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
