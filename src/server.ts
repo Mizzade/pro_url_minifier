6bda31c3e3a16ebf1cfff987ee80aee2887d67e7
Mizzade <bleiwolle@gmail.com>
2025-07-09 15:02:49 +0200

    fix: correct endpoint types
    feature: add global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { nanoid } from "nanoid";
+import type { NextFunction, Request, Response } from "express";
+
+const nanoidAsync = async (length: number): Promise<string> =>
+  Promise.resolve(nanoid(length));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,19 +14,35 @@ app.get("/", (_, res) => {
   res.send("Hello from Express and Bun!");
 });
 
-// TODO: Fix types for this endpoint
-app.post("/api/shorten", async (req, res) => {
-  const { url }: { url: string } = req.body;
+app.post(
+  "/api/shorten",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { url }: { url: string } = req.body || {};
 
-  if (!url) {
-    return res.status(400).json({ error: "URL is required" });
+    if (!url) {
+      res.status(400);
+      const error = new Error("Missing URL in request body.");
+      return next(error);
+    }
+
+    try {
+      const shortUrl = await nanoidAsync(10);
+      res.json({ shortUrl });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+// Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.statusCode < 400) {
+    res.status(500);
   }
 
-  const shortUrl = await nanoidAsync(10);
-  res.json({ shortUrl });
+  const message = err.message || "Internal Server Error";
+
+  res.json({ error: { message } });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-const nanoidAsync = async (length: number): Promise<string> =>
-  Promise.resolve(nanoid(length));
