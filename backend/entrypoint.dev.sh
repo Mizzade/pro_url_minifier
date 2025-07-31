ee092f35274284d4831832a4e9d7e1a7e1904946
Mirko Lauff <bleiwolle@gmail.com>
2025-07-31 11:19:05 +0200

    refactor: Reduce prod image size (#22)
    Reduced from 735 MB to 283 MB

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
+# TODO: Put the generate call into the Dockerfile
 echo "Generating Prisma Client..."
 PRISMA_CLI_BINARY_TARGETS="linux-arm64-openssl-3.0.x" bunx prisma generate
 
--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -3,6 +3,18 @@
 # Ensures the script exits immediately if any command fails. Good practice in production scripts.
 set -e
 
+# The prisma migrate deploy command applies database migrations, which requires:
+# - A live connection to your production database
+# - Appropriate credentials and permissions
+# But during docker build, you don’t have access to runtime secrets or the
+# target database—this phase is meant for building artifacts, not executing
+# side effects like touching a DB.
+
+# Running prisma migrate deploy during the build introduces:
+# - Side effects (writes to database)
+# - Dependency on environment-specific runtime configuration (e.g., production DB access)
+# - Risk of migrations being tied to image build, making the build non-repeatable
+
 echo "Generating Prisma Client..."
 PRISMA_CLI_BINARY_TARGETS="linux-arm64-openssl-3.0.x" bunx prisma generate
 
@@ -10,4 +22,4 @@ echo "Running Prisma Migrations..."
 bunx prisma migrate deploy --schema=./prisma/schema.prisma
 
 echo "Starting app..."
-exec bun ./src/server.ts
+exec bun ./server.js
